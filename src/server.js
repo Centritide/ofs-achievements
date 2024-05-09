dec1a25281c034530b90299d3faf1f32b790f3a3
Centritide <49379683+Centritide@users.noreply.github.com>
2024-05-09 05:18:03 -0400

    Migrate to supabase. fix bonerattle

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@
  */
 
 import { Router } from 'itty-router';
-import {Client,connect} from '@planetscale/database';
+import { Client } from "pg";
 import {
   InteractionResponseType,
   InteractionType,
@@ -13,7 +13,7 @@ import {UPDATE_EVENT_COMMAND,DISPLAY_PROFILE_COMMAND, IMPORT_FROM_ROLES_COMMAND,
 const data = require("./data/data.json");
 const dict = data.dict;
 const event_thresholds = data.event_thresholds;
-const table = "users_prod";
+const table = "users";
 class JsonResponse extends Response {
   constructor(body, init) {
     const jsonBody = JSON.stringify(body);
@@ -59,7 +59,6 @@ router.post('/', async (request, env) => {
   
   if (interaction.type === InteractionType.APPLICATION_COMMAND) {
     // Most user commands will come as `APPLICATION_COMMAND`.
-    // console.log("test");
     switch (interaction.data.name.toLowerCase()) {
       case DISPLAY_PROFILE_COMMAND.name.toLowerCase():
         return showProfile(interaction,env);
@@ -83,7 +82,6 @@ router.post('/', async (request, env) => {
         return requestScore(interaction,env);
       case TEST_COMMAND.name.toLowerCase():
         
-        // console.log("test");
         return new JsonResponse({
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
           data:{
@@ -116,7 +114,6 @@ router.all('*', () => new Response('Not Found.', { status: 404 }));
 async function showProfile(interaction,env){
   const user = interaction.data.options[0].value;
 
-  // console.log(interaction.data.resolved.users[user].avatar);
 
   let avi_url;
   if(interaction.data.resolved.users[user].avatar === null){
@@ -124,27 +121,27 @@ async function showProfile(interaction,env){
   }else if(interaction.data.resolved.users[user].avatar.substring(0,2) == "a_"){
     avi_url = "https://cdn.discordapp.com/avatars/"+user+"/"+interaction.data.resolved.users[user].avatar+".gif";
   }else{
-    // console.log("test")
     avi_url = "https://cdn.discordapp.com/avatars/"+user+"/"+interaction.data.resolved.users[user].avatar+".png";
   }
-  // console.log("a");
   const page = (interaction.data.options.length>=2) ? interaction.data.options[1].value : 0;
   const usemax = data.layouts[page][0];
   const title = data.layouts[page][1];
   const layoutscores = data.layouts[page][2];
-  // console.log(interaction.guild);
-  // console.log(data.layouts[page]);
-  const config = {
-    host: env.DATABASE_HOST,
-    username: env.DATABASE_USERNAME,
-    password: env.DATABASE_PASSWORD
-  }
-  const conn = connect(config);
-  const output = await conn.execute("SELECT * from " + table + " where id = ?;", [user]);
+  const client = new Client({
+    user: env.PG_USER ,
+    password: env.PG_PW ,
+    host: env.PG_HOST ,
+    port: 6543,
+    database: env.PG_NAME 
+  });
+
+  await client.connect();
+  const output = await client.query(`SELECT * from ${table} where id = ${user}`);
   let row;
   if(output.rows.length >0){
     row = output.rows[0];
   } else {
+    client.end();
     return new JsonResponse({type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
       data: {
         content: "No profile detected for user <@"+user+">",
@@ -165,16 +162,13 @@ async function showProfile(interaction,env){
           "inline":true 
         })
       }else if(typeof i === 'string'){
-        // console.log(i);
-        console.log(getStageMax(row,i));
+        // console.log(getStageMax(row,i));
         fields.push({
           "name":dict[i],
           "value":(getStageMax(row,i)==0)?"-":getStageMax(row,i),
           "inline":true
         })
       } else {
-        // console.log(i);
-        // console.log(getStageMax(row,i));
         fields.push({
           "name": "🌙"+dict[i[0]]+"/☀️",
           "value":((getStageMax(row,i[0])==0)?"-":getStageMax(row,i[0]))+"/"+((getStageMax(row,i[1])==0)?"-":getStageMax(row,i[1])),
@@ -185,19 +179,14 @@ async function showProfile(interaction,env){
   } else{
     let layout = [];
     for(let i of layoutscores){
-      // console.log(i);
-      // console.log(data.col_groups[i]);
+
       layout = layout.concat(data.col_groups[i]);
-      // console.log(data.col_groups[i].length%3);
       for(let j = 0; data.col_groups[i].length%3>0 && j<(3-(data.col_groups[i].length%3));j++){
         layout.push("");
       }
     }
-    // console.log(layout);
     for(let i of layout){
-    // console.log(i);
       const value = row[i];
-      // console.log(value);
       if(i == ""){
         fields.push({
           "name": "​", //this too
@@ -213,58 +202,7 @@ async function showProfile(interaction,env){
       }
     }
   }
-  
-  // console.log(fields);
-  // const events = ["br1", "br2","br3","br4","ew1","ew2","ew3"]
-  // let awards = "​";
-  // let first = true;
-  // for(let i of events){
-
-  //   if(row[i]>=event_thresholds[i]){
-
-  //     awards = awards + (first ? "" : ", ")+  dict[i];
-  //     first = false;
-  //   }
-  // }
-  // // console.log(awards);
-  // fields.push({
-  //   "name":"Top 5% events",
-  //   "value": awards,
-  //   "inline": false
-  // });
-  // console.log("here")
-  // const layout2 = ["s2map0", "s2map1","s2map2",
-  // "s2map0day","s2map1day","s2map2day",
-  // "s2map3","s2map4","s2princess",
-  // "s2map3day", "s2map4day",""]
-  // for(let i in layout2){
-  //   // console.log(layout[i]);
-  //   const value = row[layout2[i]];
-  //   if(value >= 150){
-  //     fields.push({
-  //       "name": dict[layout2[i]],
-  //       "value": value,
-  //       "inline":true
-  //     })
-  //   } else {
-  //     fields.push({
-  //       "name": dict[layout2[i]],
-  //       "value": "​",
-  //       "inline":true 
-  //     })
-  //   }
-  // }
-  // console.log(perms);
-  // update when added to OFS
-  const guilds = await fetch("https://discord.com/api/v10/users/@me/guilds",{headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bot ${env.DISCORD_TOKEN}`,
-  }}).then(response => response.json());
-  // const guild = guilds.find(guild => guild.id === "OFS ID")
-  // console.log(guilds); 
-  // const guild_icon = "https://cdn.discordapp.com/icons/" + guild.id + "/" + guild.icon + ".png";
-  // console.log(guild_icon);
-  // console.log(fields);
+  client.end();
   return new JsonResponse({type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
     data: {
       "content": "",
@@ -299,7 +237,6 @@ async function showProfile(interaction,env){
 }
 
 function getStageMax(row,stage){
-  // console.log("a");
   let arr = [];
   for(let i of data.col_groups[stage]){
     arr.push(row[i])
@@ -313,9 +250,7 @@ function getStageEmoji(stage,score){
 // handles responding to super secret staff buttons/fields
 
 async function componentResponse(interaction,env){
-  // console.log(interaction);
   const user = interaction.message.embeds[0].fields[0].value;
-  // console.log(user);
   let subcommand = interaction.message.embeds[0].fields[1].value;
   let score = Number(interaction.message.embeds[0].fields[2].value);
   let rot_type = interaction.message.embeds[0].fields[3].value;
@@ -333,15 +268,12 @@ async function componentResponse(interaction,env){
     })
   })
   const dmchannel = await response.json();
-  // await console.log(dmchannel.id);
   switch(interaction.data.custom_id.toLowerCase()){
     case "approve":
       
       let column = stage + ((subcommand == "bigrun" || subcommand == "eggstra") ? "":rot_type);
-      // console.log(stage);
-      // console.log();
+
       let newscore = await updateScore(user,score,column,env);
-      // console.log("hello");
       
       const acceptresponse = await fetch(`https://discord.com/api/v10/channels/${dmchannel.id}/messages`,{
         headers: {
@@ -387,7 +319,6 @@ async function componentResponse(interaction,env){
       });
     case "change subcommand":
       subcommand = interaction.data.values[0];
-      // console.log(map_event_field);
       
     case "change map":
       stage = interaction.data.values[0];
@@ -470,7 +401,6 @@ function embedMaker(link,user,subcommand,score,rot_type,stage){
 
 function mapField(subcommand){
   let map_event_field;
-  // console.log("test");
   switch(subcommand){
     case REQUEST_SCORE_COMMAND.options[0].name.toLowerCase(): //s3
       map_event_field = 
@@ -592,7 +522,6 @@ function mapField(subcommand){
       };
       break;
     case REQUEST_SCORE_COMMAND.options[3].name.toLowerCase(): //eggstra
-      // console.log("test");
       map_event_field = 
       {
         type:1,
@@ -626,7 +555,6 @@ function mapField(subcommand){
       };
       break;
   }
-  // console.log("test2" + map_event_field);
   return map_event_field;
 }
 
@@ -788,13 +716,13 @@ function componentMaker(subcommand,score,rot_type,stage){
   } else if(rot_type == "False"){
     return([rotationrow]);
   }else {
-    console.log([
-      approverow,
-      subcommandrow,
-      stagerow,
-      rotationrow,
-      scorerow
-    ]);
+    // console.log([
+    //   approverow,
+    //   subcommandrow,
+    //   stagerow,
+    //   rotationrow,
+    //   scorerow
+    // ]);
     return(
       [
         approverow,
@@ -810,71 +738,55 @@ function componentMaker(subcommand,score,rot_type,stage){
 //three functions that all update a score. well done. very normal
 
 async function updateScore(user,score,column,env,strict_increase = true){
-  const config = {
-    host: env.DATABASE_HOST,
-    username: env.DATABASE_USERNAME,
-    password: env.DATABASE_PASSWORD,
-        fetch: (url, init) => {
-        delete (init)["cache"];
-        return fetch(url, init);
-    }
-  }
-  // console.log("what");
+ 
   // console.log(user +" " + score+ " " + column);
-  // console.log("test");
-  const client = new Client(config);
-  // console.log("hmm?");
-  const conn = client.connection();
-  // const check = await conn.execute("select if(exists (select * from users where id =?),true, false);",[user],{as:'array'});
-  // let query = "INSERT INTO users (id," + column + ") VALUES (" + user + "," + score + ");";
-  // console.log(check.rows[0][0]==1);
+ 
+  const client = new Client({
+    user: env.PG_USER ,
+    password: env.PG_PW ,
+    host: env.PG_HOST ,
+    port: 6543,
+    database: env.PG_NAME 
+  });
+  try {
+    await client.connect();
+  } catch (error) {
+    console.error(error);
+  }
+
   if(strict_increase){
-    const old = await conn.execute("SELECT "+column+" from " + table + " where id = ?;", [user],{as:"array"});
-    console.log(old);
+    const old = await client.query(`SELECT ${column} from ${table} where id = ${user};`);
     let old_score = 0;
-    // console.log(old.rows.length>0);
-    if(old.rows.length>0 && (old.rows[0][0]<score)){
+    if(old.rows.length>0 && (old.rows[0][column]<score)){
       try{
-        // console.log("test");
-        console.log(await conn.execute("UPDATE " + table + " set "+ column +" = " + score + " where id = "+ user +";"));
-        old_score = old.rows[0][0];
-        // console.log(old.rows);
+        await client.query(`UPDATE ${table} set ${column} = ${score} where id = ${user};`)
+        
+        old_score = old.rows[0][column];
       } catch{
-        // console.error("what");
       }
     } else if (old.rows.length > 0){
-      old_score = old.rows[0][0];
-      console.log(old_score);
+      old_score = old.rows[0][column];
     }else {
       old_score = 0;
-      await conn.execute("INSERT INTO " + table + " (id,"+ column+ ") VALUES ("+ user +","+ score +");",{column:column,score:score,user:user});
+      await client.query(`INSERT INTO ${table} (id,${column}) VALUES (${user},${score});`);
     };
     return Math.max(old_score,score);
   } else {
     let old_score;
-    const old = await conn.execute("SELECT "+column+" from " + table + " where id = ?;", [user],{as:"array"});
-    console.log(old);
+    const old = await client.query(`SELECT ${column}from ${table} where id = ${user};`);
     if(old.rows.length <= 0){
       old_score = 0;
-      await conn.execute("INSERT INTO " + table + " (id,"+ column+ ") VALUES ("+ user +","+ score +");",{column:column,score:score,user:user});
+      await client.query(`INSERT INTO ${table} (id,${column}) VALUES (${user},${score});`);
     } else {
       try{
-        // console.log("test");
-        console.log(await conn.execute("UPDATE " + table + " set "+ column +" = " + score + " where id = "+ user +";"));
-        old_score = old.rows[0][0];
-        // console.log(old.rows);
+        console.log(await client.query(`UPDATE ${table} set ${column} = ${score} where id = ${user};`));
+        old_score = old.rows[0][column];
       } catch{
-        // console.error("what");
       }
     }
+    client.end();
     return old_score;
   }
-  // console.log(Math.max(old_score,score));
-  // try{
-  //   console.log(await conn.execute(query));
-  // }catch (e){
-  //   console.error(e);
-  // }
   
 }
 
@@ -900,16 +812,12 @@ async function updateEvent(interaction,env){
 }
 
 async function update(interaction,env){
-  // console.log(interaction);
-  // console.log("test");
   const user = interaction.data.options[0].options[0].value;
   const stage = interaction.data.options[0].options[1].value;
   const score = interaction.data.options[0].options[2].value;
   const rot_type = interaction.data.options[0].options[3].value;
   let column = stage + rot_type;
-  // console.log("here");
   const old_score = await updateScore(user,score,column,env,false);
-  // console.log(content);
   if(old_score ===undefined){
     return new JsonResponse({type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
       data: {
@@ -918,8 +826,6 @@ async function update(interaction,env){
     }});
   } else {
   let content = "Updated " + column + " score for <@"+user + "> to " + score + ". Was:"  + old_score;
-  // console.log(content);
-  // console.log(content);
   return new JsonResponse({type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
     data: {
       content: content,
@@ -930,14 +836,12 @@ async function update(interaction,env){
 // requests. sends a message to secret channel
 async function requestScore(interaction,env){
   const subcommand = interaction.data.options[0].name.toLowerCase();
-  // console.log(interaction);
   const user = interaction.member.user.id;
   const options = interaction.data.options[0].options;
   // console.log(options);
   let link, score,stage;
   let rot_type = "event";
   for(const i of options) {
-    // console.log(i);
     switch (i.name.toLowerCase()){
       case "stage":
         stage = i.value;
@@ -967,7 +871,6 @@ async function requestScore(interaction,env){
         break;
     }
   }
-  // console.log(link);
   if(link == undefined){
     return new JsonResponse({
       type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
@@ -977,59 +880,40 @@ async function requestScore(interaction,env){
       }
     });
   }
-  // const link = interaction.data.options[0].options[0].value;
-  // const stage = interaction.data.options[0].options[1].value;
-  // const score = interaction.data.options[0].options[2].value;
-  // const dayonly = (interaction.data.options.length >=4) ? interaction.data.options[3].value : false;
   const content = "<@" + user + "> requested (" + subcommand + ") " + score + " on " + stage + " " + rot_type + " " + link;
   let real;
   let attachments=[];
-  
-  // let attachments = [];
-  if(link.substring(0,29) === "https://discord.com/channels/"){
+    if(link.substring(0,29) === "https://discord.com/channels/"){
     const parts = link.split("/");
-    // console.log(parts);
     const channel_id = parts[5];
     const msg_id = parts[6];
-    // console.log(channel_id);
-    // console.log(msg_id);
     const get_message = await fetch(`https://discord.com/api/v10/channels/${channel_id}/messages/` + msg_id, {
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bot ${env.DISCORD_TOKEN}`,
       }
     }).then((response)=> response.json());
-    // console.log(get_message.attachments.length==0);
-    // for(let i of get_message.attachments){
-    //   attachments.push({"id":i.id});
-    // }
+
     if(get_message.attachments.length == 0){
-      console.log("dumb");
       real = 20040234;
     } else {
       link = get_message.attachments[0].url;
       attachments=get_message.attachments;
-      // const img = await fetch(link);
       real = 200;
-      // console.log(real);
     }
     
   } else if (link.substring(0,17) ==="https://stat.ink/"){
     const parts = link.split("/");
     const id = parts[5];
     link = `https://s3-img-gen.stats.ink/salmon/en-US/${id}.jpg`;
-    // console.log(link);
     const img = await fetch(link);
     real = await img.status;
   } else if(link.substring(0,27)=="https://cdn.discordapp.com/"){
-    // const img = await fetch(link);
-    // real = await img.status;
     real = 200; 
   } else {
     const img = await fetch(link);
     real = await img.status;
   }
-  // console.log(attachments);
   if(real==200){
 
   } else {
@@ -1048,15 +932,9 @@ async function requestScore(interaction,env){
       "url": attachments[i].url
     }})}
   }
-  // console.log(embeds);
   const test = env.DISCORD_APPLICATION_ID == "1173198500931043390";
   const channel_id = (test) ? "1142653555895971943" : "1178115595296841848";
-  // console.log(JSON.stringify({
-  //   "content": content,
-  //   "embeds": embeds,
-  //   "components":components,
-  //   "attachments":[]
-  // }));
+
   const response = await fetch(`https://discord.com/api/v10/channels/${channel_id}/messages`,{
     headers: {
       'Content-Type': 'application/json',
@@ -1102,19 +980,18 @@ async function importFromRoles(id,interaction,env){
     },
     method:'GET'
   }).then(response => response.json());
-  console.log(user.roles);
-  const config = {
-    host: env.DATABASE_HOST,
-    username: env.DATABASE_USERNAME,
-    password: env.DATABASE_PASSWORD
-  }
-  const client = new Client(config);
-  // console.log("ey");
-  const conn = client.connection();
+  // console.log(user.roles);
+  const client = new Client({
+    user: env.PG_USER ,
+    password: env.PG_PW ,
+    host: env.PG_HOST ,
+    port: 6543,
+    database: env.PG_NAME 
+  });
+  await client.connect();
   let query;
   let vals;
-  const old = await conn.execute("SELECT * from " + table + " where id = ?;", [id],{as:"array"});
-  // console.log("yo");
+  const old = await client.query(`SELECT * from ${table} where id = ${id};`);
   let hits = 0;
   if(old.rows.length <= 0){
     query = `INSERT INTO ${table} (id,`;
@@ -1130,14 +1007,14 @@ async function importFromRoles(id,interaction,env){
     query = query.replace(/,\s*$/, "");
     vals = vals.replace(/,\s*$/, "");
     query = `${query}) ${vals});`
-    // console.log("awa");
   } else {
-    console.log(old.headers);
+    // console.log(old);
     //only strictly increase event roles - if pair[1]> old.rows[0][0] or something
     query = `UPDATE ${table} set `;;
     for(let i in user.roles){
       const pair = data.roleid_mapscores[user.roles[i]];
-      if(pair!=undefined && pair[1]>old.rows[0][old.headers.indexOf(pair[0])]){
+      // console.log(pair);
+      if(pair!=undefined && pair[1]>old.rows[0][pair[0]]){
         query = query + `${pair[0]}=${pair[1]}, `;
         hits +=1;
       } 
@@ -1146,6 +1023,7 @@ async function importFromRoles(id,interaction,env){
     query = query + ` WHERE id=${id}`
   }
   if(hits<1){
+    client.end();
     return new JsonResponse({
       type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
       data: {
@@ -1154,8 +1032,9 @@ async function importFromRoles(id,interaction,env){
       }
     });
   }
-  const response = await conn.execute(query);
-  console.log(response);
+  const response = await client.query(query);
+  client.end();
+  // console.log(response);
   return new JsonResponse({
     type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
     data: {
